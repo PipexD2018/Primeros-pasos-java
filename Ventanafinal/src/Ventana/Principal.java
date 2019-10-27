bafdf9c217721c8dad91833b4011f60b71efc8f1
PipexD2018 <33470596+PipexD2018@users.noreply.github.com>
2019-10-26 23:43:16 -0500

    Ejercicio básico interfaces por codigo

---
+++ Ventanafinal/src/Ventana/Principal.java
@@ -0,0 +1,14 @@
+
+package Ventana;
+
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Ventana ve = new Ventana();
+        ve.setVisible(true);
+         //Acceso as = new Acceso();
+         //as.setVisible(true);
+    }
+}
