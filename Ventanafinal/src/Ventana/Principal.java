23c1c4dec615867535710fbbb74340b238d4a7e7
Felipe Araque <l-felipe-araque@hotmail.com>
2019-10-27 12:15:33 -0500

    Primeros-pasos-java

--- Ventanafinal/src/Ventana/Principal.java
+++ Ventanafinal/src/Ventana/Principal.java
@@ -6,9 +6,15 @@ public class Principal {
     
     public static void main(String[] args) {
         
-        Ventana ve = new Ventana();
-        ve.setVisible(true);
+        //Ventana ve = new Ventana();
+        //ve.setVisible(true);
          //Acceso as = new Acceso();
          //as.setVisible(true);
+        
+        Eventos ev = new Eventos();
+        ev.setVisible(true);
+    
+    
+    
     }
 }
